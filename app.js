0e385f0a45b37bb8c544a1cd396683d551e857e2
Max Lindquist <ml227cu@student.lnu.se>
2025-12-29 12:56:43 +0100

    create login and register pages and routes

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-import createError from 'http-errors';
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+import createError from "http-errors";
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
 
 // Database setup
-import 'dotenv/config';
-import pool from './db.js';
+import "dotenv/config";
+import pool from "./db.js";
 
-import indexRouter from './routes/index.js';
-import usersRouter from './routes/users.js';
-import booksRouter from './routes/books.js';
+import indexRouter from "./routes/index.js";
+import usersRouter from "./routes/users.js";
+import booksRouter from "./routes/books.js";
+import registerRouter from "./routes/register.js";
+import loginRouter from "./routes/login.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -19,19 +21,19 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Test database connection
-app.get('/test-db', async (req, res) => {
+app.get("/test-db", async (req, res) => {
   try {
-    const [rows] = await pool.execute('SELECT * FROM books LIMIT 10');
+    const [rows] = await pool.execute("SELECT * FROM books LIMIT 10");
     console.log(rows);
     res.json(rows);
   } catch (error) {
@@ -40,9 +42,11 @@ app.get('/test-db', async (req, res) => {
   }
 });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/books', booksRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/books", booksRouter);
+app.use("/register", registerRouter);
+app.use("/login", loginRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -53,11 +57,11 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 export default app;
---
+++ routes/register.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.render('users/register');
+});
+
+export default router;
